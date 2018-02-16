9a1c1d1695c3fad956fab3c2d3b149446100ab41
George Strauch <35750484+gstrauch2@users.noreply.github.com>
2018-02-15 18:53:06 -0600

    Update main.cpp

--- payStub/main.cpp
+++ payStub/main.cpp
@@ -10,36 +10,36 @@ using namespace std;
 
 int main() {
 
-    string name = "George Strauch";
-    double gross = 20000.00;
-    double fedTax = 0.0;    // 15%
-    double stateTax = 0.0;  // 0%
-    double ssTax = 0.0;     // 5.75%
-    double medTax = 0.0;    // 2.75%
-    double pension = 0.0;   // 5%
-    double health = 0.0;    // 5%
-    double net = 0.0;
+    string strName = "George Strauch";
+    double dblGross = 20000.00;
+    double dblFedTax = 0.0;    // 15%
+    double dblStateTax = 0.0;  // 0%
+    double dblssTax = 0.0;     // 5.75%
+    double dblMedTax = 0.0;    // 2.75%
+    double dblPension = 0.0;   // 5%
+    double dblHealth = 0.0;    // 5%
+    double dblNet = 0.0;
 
-    fedTax = (gross/100)*15;
-    stateTax = (gross/100)*0;
-    ssTax = (gross/100)*5.75;
-    medTax = (gross/100)*2.75;
-    pension = (gross/100)*5;
-    health = 75;
+    dblFedTax = (dblGross/100)*15;
+    dblStateTax = (dblGross/100)*0;
+    dblssTax = (dblGross/100)*5.75;
+    dblMedTax = (dblGross/100)*2.75;
+    dblPension = (dblGross/100)*5;
+    dblHealth = 75;
 
-    net = gross - fedTax - stateTax - ssTax - medTax - pension - health;
+    dblNet = dblGross - dblFedTax - dblStateTax - dblssTax - dblMedTax - dblPension - dblHealth;
 
     cout << fixed;
     cout << setprecision(2);
 
-    cout << left << name << endl;
-    cout << setw(30) << "gross pay" << setfill('.') << "$" << gross << endl;
-    cout << setw(30) <<"Federal tax" << setfill('.') << "$" << fedTax << endl;
-    cout << setw(30) << "State tax" << setfill('.') << "$" << stateTax << endl;
-    cout << setw(30) << "Social security tax" << setfill('.') << "$" << ssTax << endl;
-    cout << setw(30) << "Pension plan" << setfill('.') << "$" << pension << endl;
-    cout << setw(30) << "Health insurance" << setfill('.') << "$" << health << endl;
-    cout << setw(30) << "Net pay" << setfill('.') << "$" << net << endl;
+    cout << left << strName << endl;
+    cout << setw(30) << "dblGross pay" << setfill('.') << "$" << dblGross << endl;
+    cout << setw(30) <<"Federal tax" << setfill('.') << "$" << dblFedTax << endl;
+    cout << setw(30) << "State tax" << setfill('.') << "$" << dblStateTax << endl;
+    cout << setw(30) << "Social security tax" << setfill('.') << "$" << dblssTax << endl;
+    cout << setw(30) << "Pension plan" << setfill('.') << "$" << dblPension << endl;
+    cout << setw(30) << "Health insurance" << setfill('.') << "$" << dblHealth << endl;
+    cout << setw(30) << "Net pay" << setfill('.') << "$" << dblNet << endl;
 
 
 
